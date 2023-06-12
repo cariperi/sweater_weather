94560391ba3dc9634d2c5dc19324b64ac1cae774
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-11 23:21:10 -0400

    chore: update controller with check_for_location callback for search method

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,6 +1,16 @@
 class Api::V0::ForecastController < ApplicationController
+  before_action :check_for_location, only: [:search]
+
   def search
     forecast = ForecastFacade.new.get_forecast(params[:location])
     render json: ForecastSerializer.new(forecast)
+  end
+
+  private
+
+  def check_for_location
+    return unless params[:location].empty?
+
+    render json: { errors: [{ detail: 'No forecast information is available for the requested location.' }] }, status: 400
   end
 end
