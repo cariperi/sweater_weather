1049e0304117cf5da76bb1650cd3197ecf801eab
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-12 12:06:02 -0400

    test: create library service spec and test for get_books method

---
+++ spec/services/library_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe LibraryService do
+  it 'returns data about documents matching a keyword', :vcr do
+    service = LibraryService.new
+    response = service.get_books('Denver, CO', '5')
+
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:start)
+    expect(response).to have_key(:num_found)
+    expect(response).to have_key(:docs)
+
+    expect(response[:docs]).to be_an(Array)
+    expect(response[:docs].count).to eq(5)
+  end
+end
