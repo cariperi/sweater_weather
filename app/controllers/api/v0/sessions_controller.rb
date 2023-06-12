d0db68c135ba9eb4c13f56f4a308c43a1c080c36
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-12 18:10:07 -0400

    chore/refactor: add error handling to sessions create method and refactor with callbacks and helpers

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -1,9 +1,29 @@
 class Api::V0::SessionsController < ApplicationController
-  def create
-    user = User.find_by(email: params[:email].downcase)
+  before_action :check_params, only: [:create]
+  before_action :find_user, only: [:create]
 
-    if user.authenticate(params[:password])
-      render json: UserSerializer.new(user), status: 200
-    end
+  def create
+    @user.authenticate(params[:password]) ? render_valid_user_response : render_credential_error
+  end
+
+  private
+
+  def check_params
+    return unless params[:email].nil? || params[:password].nil?
+
+    render json: { errors: [{ detail: 'All fields must be provided.' }] }, status: 400
+  end
+
+  def find_user
+    @user = User.find_by(email: params[:email].downcase)
+    render_credential_error if @user.nil?
+  end
+
+  def render_credential_error
+    render json: { errors: [{ detail: 'The credentials provided are not valid.' }] }, status: 401
+  end
+
+  def render_valid_user_response
+    render json: UserSerializer.new(@user), status: 200
   end
 end
