85385014b21a0c911a991fae3a49ad87e33667f6
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-12 11:55:01 -0400

    test: add test for library facade get_books method

---
+++ spec/facades/library_facade_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe LibraryFacade, :vcr do
+  describe 'instance methods' do
+    describe 'get_books(location, quantity)' do
+      it 'returns a hash with total books and a books array' do
+        location = 'Denver, CO'
+        quantity = '5'
+
+        result = LibraryFacade.new.get_books(location, quantity)
+
+        expect(result).to be_a(Hash)
+        expect(result).to have_key(:total_books_found)
+        expect(result[:total_books_found]).to be_an(Integer)
+        expect(result).to have_key(:books)
+        expect(result[:books]).to be_an(Array)
+
+        books = result[:books]
+        expect(books.count).to eq(5)
+        books.each do |book|
+          expect(book).to be_a(Hash)
+          expect(book).to have_key(:isbn)
+          expect(book[:isbn]).to be_an(Array)
+          expect(book).to have_key(:title)
+          expect(book[:title]).to be_a(String)
+          expect(book).to have_key(:publisher)
+          expect(book[:publisher]).to be_an(Array)
+        end
+      end
+    end
+  end
+end
