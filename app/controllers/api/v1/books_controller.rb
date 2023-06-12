8777efb67f9ab23226c014837ec93f758287dbf9
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-12 13:45:07 -0400

    chore: update controller with callback to check for location and return an error if not found

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::BooksController < ApplicationController
+  before_action :check_for_location, only: [:search]
 
   def search
     book_search = BookSearchFacade.new.get_details(params[:location], params[:quantity])
     render json: BookSearchSerializer.new(book_search)
+  end
+
+  private
+
+  def check_for_location
+    return unless params[:location].empty?
+
+    render json: { errors: [{ detail: 'Search cannot be completed with the given information.' }] }, status: 400
   end
 end
