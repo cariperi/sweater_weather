e3e4d683acf73f116e419f165a500f99b48f2f4d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-13 00:25:54 -0400

    WIP Road Trip Facade

---
+++ app/facades/road_trip_facade.rb
@@ -0,0 +1,26 @@
+class RoadTripFacade
+  def get_road_trip(origin, destination)
+    travel_time = get_travel_time(origin, destination)
+    weather = get_destination_forecast(destination, get_eta(destination, travel_time))
+
+    RoadTrip.new(origin, destination, travel_time, weather)
+  end
+
+  private
+
+  def get_travel_time(origin, destination)
+    DirectionsFacade.new.get_travel_time(origin, destination)
+  end
+
+  def get_destination_forecast(destination, eta)
+    WeatherFacade.new.get_destination_forecast(destination, eta)
+  end
+
+  def get_eta(destination, time)
+    coordinates = GeocodeFacade.new.get_coordinates(destination)
+    timezone = Timezone.lookup(coordinates[:lat], coordinates[:lon])
+
+    eta = timezone.utc_to_local(Time.now) + time
+    { date: eta.strftime('%F'), hour: eta.strftime('%H') }
+  end
+end
