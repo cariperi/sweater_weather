ff7378131c33ad280982d3454ef4bbc322d10495
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-13 10:17:15 -0400

    chore: create road trip poro and initialize method

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,19 @@
+class RoadTrip
+  attr_reader :id,
+              :start_city,
+              :end_city,
+              :travel_time,
+              :weather_at_eta
+
+  def initialize(origin, destination, time, weather)
+    @id = nil
+    @start_city = origin
+    @end_city = destination
+    @travel_time = format_time(time)
+    @weather_at_eta = weather
+  end
+
+  def format_time(time)
+    "#{time.div(3600)}h #{(time % 3600).div(60)}m"
+  end
+end
