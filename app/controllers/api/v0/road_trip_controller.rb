e5b99a7c90608ed0b31bab39401958306528fe08
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-13 11:19:28 -0400

    refactor: move shared error response methods to application controller

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -1,5 +1,6 @@
 class Api::V0::RoadTripController < ApplicationController
   before_action :verify_api_key, only: [:create]
+  before_action :check_params, only: [:create]
 
   def create
     road_trip = RoadTripFacade.new.get_road_trip(params[:origin], params[:destination])
@@ -15,7 +16,9 @@ class Api::V0::RoadTripController < ApplicationController
     render_unauthorized_response
   end
 
-  def render_unauthorized_response
-    render json: { errors: [{ detail: 'Unauthorized.' }] }, status: 401
+  def check_params
+    return unless params[:origin].nil? || params[:destination].nil?
+
+    render_missing_params_response
   end
 end
--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V0::SessionsController < ApplicationController
   before_action :find_user, only: [:create]
 
   def create
-    @user.authenticate(params[:password]) ? render_valid_user_response : render_credential_error
+    @user.authenticate(params[:password]) ? render_valid_user_response : render_unauthorized_response
   end
 
   private
@@ -11,16 +11,12 @@ class Api::V0::SessionsController < ApplicationController
   def check_params
     return unless params[:email].nil? || params[:password].nil?
 
-    render json: { errors: [{ detail: 'All fields must be provided.' }] }, status: 400
+    render_missing_params_response
   end
 
   def find_user
     @user = User.find_by(email: params[:email].downcase)
-    render_credential_error if @user.nil?
-  end
-
-  def render_credential_error
-    render json: { errors: [{ detail: 'The credentials provided are not valid.' }] }, status: 401
+    render_unauthorized_response if @user.nil?
   end
 
   def render_valid_user_response
