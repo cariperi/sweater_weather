8ea4c3241a62f57f24a928e5bf9bd6845946e455
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-13 10:16:57 -0400

    test: add initialization and attribute tests for road trip poro

---
+++ spec/poros/road_trip_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe RoadTrip do
+  describe 'initialization' do
+    it 'exists and has attributes' do
+      origin = 'Cincinnati, OH'
+      destination = 'Chicago, IL'
+      time = 7980
+      weather = { datetime: '2023-04-07 23:00', temperature: 44.2, condition: 'Cloudy' }
+
+      road_trip = RoadTrip.new(origin, destination, time, weather)
+
+      expect(road_trip).to be_a(RoadTrip)
+      expect(road_trip.start_city).to be_a(String)
+      expect(road_trip.end_city).to be_a(String)
+      expect(road_trip.travel_time).to be_a(String)
+      expect(road_trip.weather_at_eta).to be_a(Hash)
+
+      weather = road_trip.weather_at_eta
+      expect(weather).to have_key(:datetime)
+      expect(weather).to have_key(:temperature)
+      expect(weather).to have_key(:condition)
+    end
+  end
+end
