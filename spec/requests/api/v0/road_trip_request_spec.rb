88ccb0e43951eba5fb080a169e0ac1334305283a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-06-13 10:54:13 -0400

    test: add happy path test for impossible route

--- spec/requests/api/v0/road_trip_request_spec.rb
+++ spec/requests/api/v0/road_trip_request_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe 'Road Trip Request' do
         user.api_keys.create(token: 'test_key')
 
         params = { origin: 'Cincinnati, OH',
-                  destination: 'Chicago, IL',
-                  api_key: 'test_key' }
+                   destination: 'Chicago, IL',
+                   api_key: 'test_key' }
 
         post('/api/v0/road_trip', params:)
 
@@ -59,8 +59,60 @@ RSpec.describe 'Road Trip Request' do
       end
     end
 
-    xit 'returns roadtrip data with expected values when travel is impossible' do
+    it 'returns roadtrip data with expected values when travel is impossible' do
+      VCR.use_cassette('request_happy_path_2', allow_playback_repeats: true) do
+        user = User.create(email: 'whatever@example.com', password: 'password', password_confirmation: 'password' )
+        user.api_keys.create(token: 'test_key')
 
+        params = { origin: 'New York, NY',
+                   destination: 'London, England',
+                   api_key: 'test_key' }
+
+        post('/api/v0/road_trip', params:)
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+        expect(data).to be_a(Hash)
+        expect(data).to have_key(:data)
+
+        road_trip = data[:data]
+        expect(road_trip).to be_a(Hash)
+
+        expect(road_trip).to have_key(:id)
+        expect(road_trip[:id]).to be_a(String)
+        expect(road_trip[:id]).to eq('null')
+
+        expect(road_trip).to have_key(:type)
+        expect(road_trip[:type]).to be_a(String)
+        expect(road_trip[:type]).to eq('road_trip')
+
+        expect(road_trip).to have_key(:attributes)
+        expect(road_trip[:attributes]).to be_a(Hash)
+
+        attributes = road_trip[:attributes]
+        expect(attributes).to have_key(:start_city)
+        expect(attributes[:start_city]).to be_a(String)
+        expect(attributes[:start_city]).to eq('New York, NY')
+
+        expect(attributes).to have_key(:end_city)
+        expect(attributes[:end_city]).to be_a(String)
+        expect(attributes[:end_city]).to eq('London, England')
+
+        expect(attributes).to have_key(:travel_time)
+        expect(attributes[:travel_time]).to be_a(String)
+        expect(attributes[:travel_time]).to eq('Impossible route.')
+
+        expect(attributes).to have_key(:weather_at_eta)
+        expect(attributes[:weather_at_eta]).to be_a(Hash)
+
+        weather = attributes[:weather_at_eta]
+        expect(weather).to be_empty
+        expect(weather).to_not have_key(:datetime)
+        expect(weather).to_not have_key(:temperature)
+        expect(weather).to_not have_key(:condition)
+      end
     end
   end
 
